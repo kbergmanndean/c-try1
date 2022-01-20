f74ca2f3fc10b6eb0fa9eb23f9d3677b11e01ea7
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-20 18:44:06 -0500

    added fallback root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
