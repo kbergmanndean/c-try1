576f658f10f6daa40cfa9cc4bd802449fec2f71f
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-20 18:12:30 -0500

    changed post fetch destination to heroku

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -11,7 +11,7 @@ function Login(){
             username: username,
             password: password
         }
-        const res = await fetch ("http://localhost:3000/login",{
+        const res = await fetch ("https://cathybergmann.herokuapp.com/login",{
             method: "POST",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify({user})
