4a76b2ad83f6372df89e75f43bf3c9a3d5a80a82
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-21 15:46:00 -0500

    path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     include ActionController::Cookies
 
   def fallback_index
-    render '/client/public/index.html'
+    render template:'/client/public/index.html'
   end
 
 
