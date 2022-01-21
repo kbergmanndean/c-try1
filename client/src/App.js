843a6c3902fa2b3f8fe194f7ece5c490e3d63b0c
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-21 12:44:26 -0500

    changed location of browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import Home from "./Components/Home.js"
 import Login from "./Components/Login.js"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Router>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route exact path="log-in" element={<Login/>}/>
         </Routes>
-      </Router>
     </div>
   );
 }
