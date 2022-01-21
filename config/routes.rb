a840609d9b31f95a055e089141a0cf2c77833697
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-21 13:30:16 -0500

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'fallback#index'
   resources :items, only: [:index, :show, :create, :destroy]
   resources :users, only: [:index, :show]
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '*path', to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
