8bd8ed2a321c9624f3567259af688668183cc969
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-21 15:03:24 -0500

    changed render file to render

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root 'application#fallback_index'
+  # root 'application#fallback_index'
   resources :items, only: [:index, :show, :create, :destroy]
   resources :users, only: [:index, :show]
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  # get '*path', to: 'application#fallback_index', constraints: ->(request) do
-  #   !request.xhr? && request.format.html?
-  # end
+  get '*path', to: 'application#fallback_index', constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
   # get '*path', to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
