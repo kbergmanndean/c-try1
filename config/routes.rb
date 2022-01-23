d73c14c0bb926868ceca0b76d0c5649b3b777f7c
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-23 13:34:11 -0500

    changed fallback route

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   get "/", to: 'application#fallback_index_html'
-  get '*path', to: 'application#fallback_index_html', constraints: ->(request) do
-    !request.xhr? && request.format.html?
+  get '*path', to: 'application#fallback_index_html', constraints: ->(request) {!request.xhr? && request.format.html? }
   end
-  # get '*path', to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
